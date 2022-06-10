20a236ac75b85b8d550f4828e925934f7d86d5a4
robermau <robertom.perez@outlook.com>
2022-06-10 15:13:10 -0300

    frontend + back v1

--- frontend/src/pages/Index.js
+++ frontend/src/pages/Index.js
@@ -1,9 +1,9 @@
 import Carrousel from '../components/Carrousel';
-import {Link as LinkRouter} from 'react-router-dom';
+import { Link as LinkRouter } from 'react-router-dom';
 
 
 const navigation = [
- 
+
   { name: "Cities", to: "/cities", current: false },
   ,
 ];
@@ -11,8 +11,8 @@ const navigation = [
 export default function Index() {
   return (
     <main className="mainContainer">
-     
-        
+
+
       <div className="main ">
         <img className="logo" src="https://imge.cloud/images/2022/06/07/rKUUz8.png"
           alt="logoportada"
@@ -22,23 +22,23 @@ export default function Index() {
           your perfect trip, designed by insiders who know and love
           their cities!".
         </p>
-     
-      </div>
-     
-      <div className="sencond-banner">
-      <h1> Discover stories, Travel in time </h1>
-      <h2> If you want to travel in the time </h2>
-      {navigation.map((item) => (
-        <LinkRouter to={item.to}  key={item.name} 
-      className='callToAction'> TRAVEL HERE!!
-     </LinkRouter>
-      ))}
-<h3>POPULAR MYTINERARY</h3>
-      <Carrousel />
-       
-     </div>
 
-       
+      </div>
+
+      <div className="sencond-banner">
+        <h1> Discover stories, Travel in time </h1>
+        <h2> If you want to travel in the time </h2>
+        {navigation.map((item) => (
+          <LinkRouter to={item.to} key={item.name}
+            className='callToAction'> TRAVEL HERE!!
+          </LinkRouter>
+        ))}
+        <h3>POPULAR MYTINERARY</h3>
+        <Carrousel />
+
+      </div>
+
+
     </main>
   )
 }
